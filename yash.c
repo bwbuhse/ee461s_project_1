702a65b2a5e555c4a32490d1642a9302f3a7bd9a
bwbuhse <bwbuhse@utexas.edu>
2020-01-25 23:14:35 -0600

    Renamed main.c to yash.c

---
+++ yash.c
@@ -0,0 +1,15 @@
+#include <readline/readline.h>
+
+int main()
+{
+    int cpid;
+    // char cmd[80];
+
+    while (1)
+    {
+        char *cmd = readline("#");
+        // printf(cmd);
+    }
+
+    return 0;
+}
