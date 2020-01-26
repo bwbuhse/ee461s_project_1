907c1e8711f299c702f0ab2db47ecd7837da2bca
bwbuhse <bwbuhse@utexas.edu>
2020-01-26 00:39:47 -0600

    YASH now can run commands without args!

--- main.c
+++ main.c
@@ -1,14 +1,28 @@
 #include <readline/readline.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
 
 int main()
 {
     int cpid;
     // char cmd[80];
 
+    // Main command loop
     while (1)
     {
-        char *cmd = readline("#");
-        printf("%s\n", cmd);
+        // Read the command into the cmd string
+        char *cmd = readline("# ");
+
+        // TODO: Tokenize the command
+
+        // Fork
+        cpid = fork();
+        if (cpid == 0)
+        {
+            // child code
+            execlp(cmd, cmd, (char *)NULL);
+        }
     }
 
     return 0;
