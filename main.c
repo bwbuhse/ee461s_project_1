deb81123a6dfb16920c414f855708d3adcbb6844
bwbuhse <bwbuhse@utexas.edu>
2020-01-27 21:56:41 -0600

    Change indentation

--- main.c
+++ main.c
@@ -4,39 +4,39 @@
 #include <string.h>
 
 int main() {
-    int cpid;
-    // char cmd[80];
+  int cpid;
+  // char cmd[80]
 
-    // Main command loop
-    while (1) {
-        // Read the command into the cmd string
-        char *cmd = readline("# ");
+  // Main command loop
+  while (1) {
+    // Read the command into the cmd string
+    char *cmd = readline("# ");
 
-        // Tokenize the command
-        char *cmdArgv[70];
-        int cmdArgc = 0;
-        char *token;
+    // Tokenize the command
+    char *cmdArgv[70]; // I used 70 because 3000/20~=70 :)
+    int cmdArgc = 0;
+    char *token;
 
-        /* get the first token */
-        token = strtok(cmd, " ");
+    /* get the first token */
+    token = strtok(cmd, " ");
 
-        /* walk through other tokens */
-        while (token != NULL) {
-            cmdArgv[cmdArgc++] = token;
+    /* walk through other tokens */
+    while (token != NULL) {
+      cmdArgv[cmdArgc++] = token;
 
-            token = strtok(NULL, " ");
-        }
-
-        // Add a NULL to the end of the argv array
-        cmdArgv[cmdArgc] = NULL;
-
-        // Fork
-        cpid = fork();
-        if (cpid == 0) {
-            // child code
-            execvp(cmdArgv[0], cmdArgv);
-        }
+      token = strtok(NULL, " ");
     }
 
-    return 0;
+    // Add a NULL to the end of the argv array
+    cmdArgv[cmdArgc] = NULL;
+
+    // Fork
+    cpid = fork();
+    if (cpid == 0) {
+      // child code
+      execvp(cmdArgv[0], cmdArgv);
+    }
+  }
+
+  return 0;
 }
