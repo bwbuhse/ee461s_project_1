7b6cbdf0250da2dba167045c9c442445b8c1070f
bwbuhse <bwbuhse@utexas.edu>
2020-01-30 21:32:18 -0600

    Get output redirection workinggit statusgit status

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
+#include <fcntl.h>
 #include <readline/readline.h>
+#include <stdbool.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -8,6 +11,23 @@
 // TODO: Add file redirects     [ ]
 // TODO: Add pipes              [ ]
 // TODO: Add signal handling    [ ]
+
+// Some macros used for keeping code nice
+#define PROMPT "# "
+#define OUTPUT_REDIR ">"
+#define INPUT_REDIR "<"
+#define ERR_REDIR "2>"
+// #define PIPE "|"
+
+// A struct used for holding parsed versions of commands
+typedef struct tokenized_cmd {
+  char **argv; // Args for execvp
+  char *output_file;
+  char *input_file;
+  char *error_file;
+  // bool pipe; // bool set to true when the command is part of a pipe
+
+} tokenized_cmd;
 
 // Used to tokenize the user's input
 // Returns the total number of tokens
@@ -18,22 +38,65 @@ int main() {
   int status;
 
   // Main command loop
-  while (1) {
+  while (true) {
+    // Flag used for if erros are found
+    bool found_error = false;
+
     // Read the input into the string and then tokenize it
-    char *input = readline("# ");
+    char *input = readline(PROMPT);
     char **tokenized_input;
     int num_tokens = tokenize(&input, &tokenized_input);
+
+    // Check for any redirections in the command
+    // cmd2 is only used if there's a pipe
+    tokenized_cmd cmd1 = {tokenized_input, NULL, NULL, NULL};
+    // tokenized cmd cmd2 = {NULL, NULL, NULL, NULL};
+    bool redirect_found = false;
+
+    for (int i = 0; i < num_tokens; i++) {
+      // Check for various file redirections
+      if (!strcmp(tokenized_input[i], OUTPUT_REDIR)) {
+        redirect_found = true;
+
+        if (i + 1 < num_tokens) {
+          cmd1.output_file = tokenized_input[i + 1];
+        } else {
+          // The input was invalid if i >= num_tokens here (ended cmd w >)
+          found_error = true;
+          break;
+        }
+      }
+
+      // If a redirect hasn't been found yet, we know we're still adding args
+      if (redirect_found) {
+        tokenized_input[i] = NULL;
+      }
+    }
+
+    // If I found an error then there's no point in trying the command
+    if (found_error) {
+      continue;
+    }
 
     // Fork
     cpid = fork();
     if (cpid == 0) {
       // child code
+      // Do any redirects
+      if (cmd1.output_file) {
+        int ofd = open(cmd1.output_file, O_CREAT | O_WRONLY | O_TRUNC,
+                       S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+        dup2(ofd, STDOUT_FILENO);
+      }
+
       execvp(tokenized_input[0], tokenized_input);
+    } else {
+      // Parent code
     }
 
     // Wait for the child processes to finish
     // TODO: Update this whenever I add background processes
-    waitpid(-1, &status, 0);
+    waitpid(cpid, &status, 0);
   }
 
   return 0;
